38fc779d65de0ce8fa902ff0aa426fc4ae2bcdc3
uyaly <uyaly@qq.com>
2017-07-14 17:55:47 +0800

    发短信ok

--- study/2.12 单选框和复选框.py
+++ study/2.12 单选框和复选框.py
@@ -4,13 +4,17 @@ url = "file:///F:/PycharmProjects/test/eg/2_12.html"
 driver = webdriver.Firefox()
 driver.get(url)
 driver.implicitly_wait(10)
-# 单选框，先选男孩，等10秒再选女孩
+# 没点击操作前，判断选项框状态
+s = driver.find_element_by_id("boy").is_selected()
+print(s)
 driver.find_element_by_id("boy").click()
-driver.implicitly_wait(10)
-driver.find_element_by_id("girl").click()
-# 复选框，全选
-checkboxs = driver.find_element_by_xpath(".//*[@type='checkbox']")
+# 没点击操作前，判断选项框状态
+r = driver.find_element_by_id("boy").is_selected()
+print(r)
+# 复选框 单选
+driver.find_element_by_id("c1").click()
+# 复选框，全选，find_elements
+checkboxs = driver.find_elements_by_xpath(".//*[@type='checkbox']")
 for i in checkboxs:
-    # checkboxs.is_selected() == "false"
+    # if i.is_selected() == "false":
     i.click()
-
