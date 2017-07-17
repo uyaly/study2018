9fdf0b5dc878ce14887352e2fdfab41087452b69
uyaly <uyaly@qq.com>
2017-07-17 11:15:07 +0800

    2.12 复选框的反选改为全选

--- study/2.12 单选框和复选框.py
+++ study/2.12 单选框和复选框.py
@@ -6,15 +6,16 @@ driver.get(url)
 driver.implicitly_wait(10)
 # 没点击操作前，判断选项框状态
 s = driver.find_element_by_id("boy").is_selected()
-print(s)
+# print(s)
 driver.find_element_by_id("boy").click()
 # 没点击操作前，判断选项框状态
 r = driver.find_element_by_id("boy").is_selected()
-print(r)
+# print(r)
 # 复选框 单选
 driver.find_element_by_id("c1").click()
 # 复选框，全选，find_elements
 checkboxs = driver.find_elements_by_xpath(".//*[@type='checkbox']")
 for i in checkboxs:
-    # if i.is_selected() == "false":
-    i.click()
+    # 全选，不执行这句是反选
+    if not i.is_selected():
+        i.click()
