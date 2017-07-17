b4da20e28546ebdc940c6795deecc62429f7b520
uyaly <uyaly@qq.com>
2017-07-17 11:18:48 +0800

    2.12 复选框的反选改为全选

--- study/2.15 富文本（richtext）.py
+++ study/2.15 富文本（richtext）.py
@@ -1,22 +1,17 @@
 # coding:utf-8
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
-# 配置文件地址
-profile_directory = r'C:\Users\Administrator\AppData\Roaming\Mozilla\Firefox\Profiles\331b4lm9.default'
-# 加载配置配置
-profile = webdriver.FirefoxProfile(profile_directory)
-# 启动浏览器配置
+profileDir = r'C:\Users\think_uyaly\AppData\Roaming\Mozilla\Firefox\Profiles\p7wm71lv.default'
+profile = webdriver.FirefoxProfile(profileDir)
 driver = webdriver.Firefox(profile)
-
-bolgurl = "http://www.cnblogs.com"
-yoyobolg = bolgurl + "yoyoketang"
-driver.get(yoyobolg)
+bolgurl = "http://www.cnblogs.com/"
+uyalybolg = bolgurl + "uyaly"
+driver.get(uyalybolg)
 driver.find_element_by_id("blog_nav_newpost").click()
 time.sleep(5)
-edittile = u'Selenium2+python自动化23-富文本'
-editbody = u"这里是发帖的正文"
+edittile = u'Selenium2+python自动化'
+editbody = u"这里是正文内容"
 driver.find_element_by_id("Editor_Edit_txbTitle").send_keys(edittile)
 driver.switch_to.frame("Editor_Edit_EditorBody_ifr")
-# 有些小伙伴可能输入不成功，可以在输入之前先按个table键，send_keys(Keys.TAB)
-# driver.find_element_by_id("tinymce").send_keys(Keys.TAB)
 driver.find_element_by_id("tinymce").send_keys(editbody)
