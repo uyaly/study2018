80081209a42bb650c2f02f0effbc066a82be5c56
uyaly <uyaly@qq.com>
2017-08-07 21:47:47 +0800

    学习了2.22 js处理富文本，成功

--- study/2.22 JS处理富文本.py
+++ study/2.22 JS处理富文本.py
@@ -1,5 +1,7 @@
 # coding:utf-8
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 # 配置文件地址
 profile_directory = r'C:\Users\Administrator\AppData\Roaming\Mozilla\Firefox\Profiles\331b4lm9.default'
 # 加载配置配置
@@ -10,12 +12,15 @@ blogurl = "http://www.cnblogs.com/"
 uyalyblog = blogurl + "uyaly"
 driver.get(uyalyblog)
 driver.find_element_by_id("blog_nav_newpost").click()
+time.sleep(5)
 
+edittile = u"Selenium2+python自动化23-富文本"
+editbody = u"这里是发帖的正文"
+driver.find_element_by_id("Editor_Edit_txbTitle").send_keys(edittile)
 body = "这里是通过js发的正文内容"
-
 # js处理iframe问题（js代码太长了，我分成两行了）
-js = 'document.getElementByID("Editor_Edit_EditorBody_ifr")'\
-     '.contentWindow.document.body.innerHTML="%s"' % body
+js = 'document.getElementById("Editor_Edit_EditorBody_ifr")' \
+    '.contentWindow.document.body.innerHTML="%s"' % body
 driver.execute_script(js)
 # 保存草稿
 driver.find_element_by_id("Editor_Edit_lkbDraft").click()
