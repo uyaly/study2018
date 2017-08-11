ed8db8aa6bc70579fc7137adfe59e5ad93e8228b
uyaly <uyaly@qq.com>
2017-08-11 16:40:25 +0800

    学习了2.31

--- study/第二章 WebDriver API/2.30 练习题2定位百度-更多产品.py
+++ study/第二章 WebDriver API/2.30 练习题2定位百度-更多产品.py
@@ -5,13 +5,16 @@ import time
 driver = webdriver.Firefox()
 driver.get("http://www.baidu.com")
 e = driver.find_element_by_link_text("更多产品")
+# 鼠标悬停,perform() 执行所有ActionChains中的行为
 ActionChains(driver).move_to_element(e).perform()
 time.sleep(1)
-ele = driver.find_element_by_name("tj_more")
+# ele = driver.find_element_by_name("tj_more")
 # 经确认，是可以定位到元素的
-print ele.text
+# print ele.text
 # 这一步点击失效了
 # ele.click()
 # js大法好，完美解决click失效问题
+# 这个name属性未找到，好奇妙的定位
 js = "document.getElementsByName('tj_more')[0].click()"
+# js = "document.getElementByClassName('s_bdbrievenmore').click()"
 driver.execute_script(js)
