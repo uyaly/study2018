66cf5fad722394e9ece5e9569b7149a28a8b9e00
uyaly <uyaly@qq.com>
2017-08-12 22:35:09 +0800

    学习2.31

--- study/第二章 WebDriver API/2.31 练习题3获取百度联系词.py
+++ study/第二章 WebDriver API/2.31 练习题3获取百度联系词.py
@@ -1,2 +1,31 @@
 # coding:utf-8
 from selenium import webdriver
+import time
+driver = webdriver.Firefox()
+driver.implicitly_wait(10)
+driver.get("http://www.baidu.com")
+time.sleep(1)
+driver.find_element_by_id("kw").send_keys(u"博客")
+# 获取百度输入框的
+time.sleep(1)
+bd = driver.find_elements_by_class_name("bdsug-overflow")
+for i in bd:
+    # 通过get_attribute()方法获取到文本信息
+    print i.get_attribute("data-key")
+# 点击其中的一个，如：第二个
+if len(bd) > 1:
+    bd[1].click()
+    # 打印当前页面url
+    print driver.current_url
+else:
+    print "未获取到匹配的词"
+
+
+# <div class="bdsug" style="height: auto; display: none;">
+# <ul>
+# <li class="bdsug-store bdsug-overflow" data-key="博客网站">
+# <li class="bdsug-store bdsug-overflow" data-key="博客">
+# <li class="bdsug-overflow" data-key="博客推广">
+# <li class="bdsug-overflow" data-key="博客天下">
+# </ul>
+# </div>
