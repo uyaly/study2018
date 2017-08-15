aeadca5d216d5968b3b23d35e5b906b3803eccf9
uyaly <uyaly@qq.com>
2017-08-15 17:03:03 +0800

    学习了3.11 捕获异常

--- lytest/case/blog/test_01.py
+++ lytest/case/blog/test_01.py
@@ -2,6 +2,8 @@
 from selenium import webdriver
 import unittest
 from login_pub import Login_Blog
+import time
+from selenium.common.exceptions import NoSuchElementException
 login_url = "https://passport.cnblogs.com/user/signin"
 class TetsLogin(unittest.TestCase):
     def setUp(self):
@@ -12,7 +14,15 @@ class TetsLogin(unittest.TestCase):
     def test_login(self):
         # 调用登录类里面的login方法
         Login_Blog(self.driver).login("uyaly", "ly612101010!")
-        # self.driver.find_element()
-        # 后面接着的操作省略了
+        time.sleep(3)
+        # 定位“名字”
+        try:
+            element = self.driver.find_element("id", "lnk_current_user_")
+        except NoSuchElementException as msg:
+            print u"查找元素异常%s"%msg
+        # 点击该元素
+        else:
+            element.click()
+
 if __name__ == "__main__":
     unittest.main()
