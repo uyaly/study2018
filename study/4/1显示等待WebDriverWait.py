88725b8467f793f9f1b99e15f5363d00b194cee0
uyaly <uyaly@qq.com>
2017-08-17 21:21:07 +0800

    4.2

--- study/4/1显示等待WebDriverWait.py
+++ study/4/1显示等待WebDriverWait.py
@@ -6,5 +6,7 @@ driver.get("http://www.baidu.com")
 # 等待时长10秒，默认0.5秒询问一次
 WebDriverWait(driver, 10).until(lambda x: x.find_element_by_id("kw")).send_keys("yoyo")
 # 判断id为kw元素是否消失
-is_disappeared = WebDriverWait(driver, 10, 1).until_not(lambda x: x.find_element_by_id("kw").is_displayed())
+# is_disappeared = WebDriverWait(driver, 10, 1).until_not(lambda x: x.find_element_by_id("kw").is_displayed())
+is_disappeared = WebDriverWait(driver, 10, 1).\
+    until_not(lambda x: x.find_element_by_id("kw").is_displayed())
 print is_disappeared
