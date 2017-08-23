e02c7b569d39031269527d4773155ed5bdd441f0
uyaly <uyaly@qq.com>
2017-08-23 10:31:13 +0800

    4.14

--- study/4/12PageObject.py
+++ study/4/12PageObject.py
@@ -1,9 +1,12 @@
 # coding:utf-8
-from xxx.yoyo_selenium import Yoyo      # 导入4.11二次封装的类
+from xxx.yoyo_selenium import Yoyo  # 导入4.11二次封装的类
+
 login_url = "https://passport.cnblogs.com/user/signin"
+
+
 class LoginPage(Yoyo):
     # 定位器，定位页面元素
-    username_loc = ("id", 'input1')     # 输入账号
+    username_loc = ("id", 'input1')  # 输入账号
     password_loc = ("id", 'input2')
     submit_loc = ("id", 'signin')
     remember_loc = ('id', 'remember_me')
@@ -11,30 +14,39 @@ class LoginPage(Yoyo):
     reset_loc = ('link text', '重置')
     register_loc = ('link text', '立即注册')
     feedback_loc = ('link text', '反馈问题')
+
     def input_username(self, username):
         '''输入账号框'''
         self.send_keys(self.username_loc, username)
+
     def input_password(self, password):
         '''输入密码框'''
         self.send_keys(self.password_loc, password)
+
     def click_submit(self):
         '''登录按钮'''
         self.click(self.submit_loc)
+
     def click_remember_live(self):
         '''下次记住登录'''
         self.click(self.remember_loc)
+
     def click_retrieve(self):
         '''找回密码'''
         self.click(self.retrieve_loc)
+
     def click_reset(self):
         '''重置密码'''
         self.click(self.reset_loc)
+
     def click_register(self):
         '''注册新账号'''
         self.click(self.register_loc)
+
     def click_feedback(self):
         '''反馈问题'''
         self.click(self.feedback_loc)
+
     def login(self, username, password):
         '''登录方法'''
         self.input_username(username)
