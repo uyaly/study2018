35086f5e9450c7dbdbae48043e3e4495024bd040
uyaly <uyaly@qq.com>
2017-08-24 17:21:54 +0800

    自动测试框架

--- test_hpk/utils/config.py
+++ test_hpk/utils/config.py
@@ -1,17 +1,18 @@
+# coding:utf-8
 import os
-from file_reader import YamlReader
+from utils.file_reader import YamlReader
 
-BAES_PATH = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + '\..')
-CONFIG_FILE = BAES_PATH + '\\config\\conf.yml'
-DATA_PATH = BAES_PATH + '\\data\\'
-DRIVER_PATH = BAES_PATH + '\\drivers\\'
-LOG_PATH = BAES_PATH + '\\report\\'
+BASE_PATH = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + '\..')
+CONFIG_FILE = BASE_PATH + '\\config\\conf.yml'
+DATA_PATH = BASE_PATH + '\\data\\'
+DRIVER_PATH = BASE_PATH + '\\drivers\\'
+LOG_PATH = BASE_PATH + '\\report\\'
 
 
 class Config:
     u'''读取配置'''
     def __init__(self, config=CONFIG_FILE):
-        self.config = YamlReader(config).data
+        self.config = YamlReader(config)._data
 
     def get(self, element, index=0):
         return self.config[index].get(element)
--- test_hpk/utils/file_reader.py
+++ test_hpk/utils/file_reader.py
@@ -6,10 +6,11 @@ import os
 class YamlReader:
     u'''封装一个YamlReader类'''
     def __init__(self, yaml):
-        if os.path.exits(yaml):
+        if os.path.exists(yaml):
             self.yaml = yaml
         else:
-            raise FileNotFoundError('文件不存在！')
+            # raise FileNotFoundError('文件不存在！')
+            print '文件不存在'
         self._data = None
 
         @property
