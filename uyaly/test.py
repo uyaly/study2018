9a03fa1d6d8953fad16927dff37362997ce4b429
uyaly <uyaly@qq.com>
2017-10-31 08:53:57 +0800

    -----

--- uyaly/test.py
+++ uyaly/test.py
@@ -1,37 +1,20 @@
 # coding:utf-8
 import unittest
-
-from pageobject.login import LoginPage
-
-from test_hpk.testcase.ly_selenium import browser
-
+from selenium import webdriver
 
 class Login_test(unittest.TestCase):
     u'''登录页面的case'''
-    def setUp(self):
-        self.driver = browser()
-        self.login= LoginPage(self.driver)      # login参数是LoginPage的实例
-        self.login.open(login_url)
-    def login_case(self, username, psw, expect=True):
+    # def setUp(self):
+        # self.driver = webdriver.Firefox()
+        # self.driver.get("http://47.52.77.154:8015/Default/Login")
+
+    def test_case(self):
         '''登录用例的方法,'''
-        # 第1步：输入账号
-        self.login.input_username(username)
-        # 第2步: 输入密码
-        self.login.input_password(psw)
-        # 第3步：点登录按钮
-        self.login.click_submit()
-        # 第4步：测试结果,判断是否登录成功
-        result = self.login.is_text_in_element(("id","lnk_current_user"),"uyaly")
-        # 第5步：期望结果
-        expect_result = expect
-        self.assertEqual(result, expect_result)
-    def test_login01(self):
-        u'''输入正确账号密码'''
-        self.login_case("uyaly", "ly612101010!", True)
-    def test_login02(self):
-        u'''输入错误账号密码'''
-        self.login_case("xx", "xx", False)
-    def tearDown(self):
-     self.driver.quit()
+        company = [11,22,33,44,55]
+        print len(company)
+        print company[0]
+        i = 0
+        for i in xrange(len(company)):
+            print company[i]
 if __name__ == "__main__":
     unittest.main()
