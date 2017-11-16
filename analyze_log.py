540317155fbfe9269fedcb78d3b637271b198e94
uyaly <uyaly@qq.com>
2017-11-16 13:41:41 +0800

    log分析

--- analyze_log.py
+++ analyze_log.py
@@ -1,25 +1,20 @@
-# coding:utf-8
-''''
-main function：主要实现把txt中的每行数据写入到excel中
-'''
-#   第一次执行的代码
 import re
+
 import argparse
 import sys
-import xlwt   # 写入文件
-import xlrd     # 打开excel文件
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file')
     args = parser.parse_args()
 
-    # if not args.file:
-    #     print 'Usage: python analyze_log -f <log_file>'
-    #     sys.exit(-1)
+    if not args.file:
+        print 'Usage: python analyze_log -f <log_file>'
+        sys.exit(-1)
 
     re_fum = re.compile('(?<= )[A-F0-9]{2}')
 
-    with open(r"D:\3.log", 'r') as f:
+    with open(args.file, 'r') as f:
         line = f.readline()
         while line:
             if not line.startswith('Report'):
@@ -66,27 +61,3 @@ if __name__ == '__main__':
                         recv_buf.extend(values)
                     if values[-1] == '7E':
                         is_sending = False
-
-# fopen = open(r"D:\3.log", 'r')
-# lines = fopen.readlines()
-# 新建一个excel文件
-file = xlwt.Workbook(encoding='utf-8', style_compression=0)
-# 新建一个sheet
-sheet = file.add_sheet('data')
-
-# 写入
-i = 0  # 行
-j = 0  # 列
-# for line in lines:
-    # if line.find('Report') != -1 or line.find('Alarm') != -1:
-    #     p = line.split(' ')
-    #     for j in range(len(p)):
-    #         # print p
-    #         if p[j] != '':
-    #             sheet.write(i, j, p[j])
-    #             j = j+1
-    #         else:
-    #             j = j+1
-    # i = i+1
-
-file.save(r'C:\Users\Administrator\Desktop\minni.xls')
