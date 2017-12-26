aa75283efdf9e7a26da8333f40d624bf9efc780f
uyaly <uyaly@qq.com>
2017-12-26 12:33:39 +0800

    -------

--- Appium/lizhi.py
+++ Appium/lizhi.py
@@ -15,12 +15,28 @@ desired_caps = {
                 }
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 # 休眠15秒等待页面加载完成
-time.sleep(5)
+time.sleep(10)
 # button = driver.find_elements_by_class_name("android.widget.LinearLayout")
 driver.find_element_by_id("com.yibasan.lizhifm:id/header_user_icon").click()
 driver.find_element_by_id("com.yibasan.lizhifm:id/followLabel").click()
 driver.find_element_by_id("com.yibasan.lizhifm:id/user_fans_user_head").click()
-
 # android.widget.TextView
-driver.find_element_by_link_text(u"声音").click()
-# driver.quit()
+# 点击【声音】
+driver.find_elements_by_class_name("android.widget.TextView")[11].click()
+# 点击【下载】
+driver.find_element_by_id("com.yibasan.lizhifm:id/btn_download").click()
+# 找未下载项，提取标题
+tittle = driver.find_elements_by_id("com.yibasan.lizhifm:id/simple_program_item_text_name")
+radio = driver.find_elements_by_id("com.yibasan.lizhifm:id/view_select_status")
+for i in range(len(tittle)):
+    if radio
+    print tittle[i].text
+    i = i+1
+# 点击【全选】
+driver.find_element_by_id("com.yibasan.lizhifm:id/view_select_all").click()
+# 点击【开始下载】
+# driver.find_element_by_id("com.yibasan.lizhifm:id/download_pop_window_done_layout").click()
+driver.quit()
+
+
+
