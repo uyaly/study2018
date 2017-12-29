77dc15512c8e5ecc5b88f30c638773927b7ee3cd
uyaly <uyaly@qq.com>
2017-12-29 16:33:29 +0800

    appnium

--- Appium/filemanage.py
+++ Appium/filemanage.py
@@ -19,12 +19,28 @@ driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 # 休眠15秒等待页面加载完成
 time.sleep(5)
 driver.find_element_by_name("手机").click()
+# 排序
+driver.find_element_by_id("com.android.fileexplorer:id/more").click()
+driver.find_element_by_name("排序").click()
+driver.find_element_by_name("名称").click()
+# 进目录
 driver.find_element_by_name("183").click()
 driver.find_element_by_name("LizhiFM").click()
 driver.find_element_by_name("Files").click()
 driver.find_element_by_name("download").click()
+# 列表排序
+driver.find_element_by_id("com.android.fileexplorer:id/more").click()
+driver.find_element_by_name("排序").click()
+driver.find_element_by_name("修改时间").click()
 # 长按文件重命名
-
+lists = driver.find_elements_by_id("com.android.fileexplorer:id/file_name")
+for i in range(len(lists)):
+    print lists[i].text
+    if lists[i].text.find("_sd.m4a") > 0:
+        driver.tap([(176, 543), (889, 601)], 3500)
+        driver.find_element_by_name("更多").click()
+        driver.find_element_by_name("重命名").click()
+        driver.find_element_by_id("com.android.fileexplorer:id/text").send_keys("Day160K.The name of the story is《Homes》解释")
 
 time.sleep(5)
 driver.quit()
--- Appium/lizhi.py
+++ Appium/lizhi.py
@@ -3,46 +3,43 @@ from appium import webdriver
 import time
 
 
-def lizhi():
-    desired_caps = {
-                    # 这里是声明android还是ios的环境
-                    'platformName': 'Android',
-                    # 手机设备名称，通过adb devices查看
-                    'deviceName': '11642f40',
-                    # android系统的版本号
-                    'platformVersion': '6.0.1',
-                    # apk包名
-                    'appPackage': 'com.yibasan.lizhifm',
-                    # apk的launcherActivity
-                    'appActivity': 'com.yibasan.lizhifm.activities.EntryPointActivity'
-                    }
-    driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
-    # 休眠15秒等待页面加载完成
-    time.sleep(10)
-    # button = driver.find_elements_by_class_name("android.widget.LinearLayout")
-    driver.find_element_by_id("com.yibasan.lizhifm:id/header_user_icon").click()
-    driver.find_element_by_id("com.yibasan.lizhifm:id/followLabel").click()
-    driver.find_element_by_id("com.yibasan.lizhifm:id/user_fans_user_head").click()
-    # android.widget.TextView
-    # 点击【声音】
-    driver.find_elements_by_class_name("android.widget.TextView")[11].click()
-    # 点击【下载】
-    driver.find_element_by_id("com.yibasan.lizhifm:id/btn_download").click()
-    # 找未下载项，提取标题
-    tittle = driver.find_elements_by_id("com.yibasan.lizhifm:id/simple_program_item_text_name")
-    radio = driver.find_elements_by_id("com.yibasan.lizhifm:id/view_select_status")
-    for i in range(len(tittle)):
-        if radio[i].is_selected() == False:
-            print tittle[i].text
-        i = i+1
-    # 点击【全选】
-    driver.find_element_by_id("com.yibasan.lizhifm:id/view_select_all").click()
-    # 点击【开始下载】
-    driver.find_element_by_id("com.yibasan.lizhifm:id/download_pop_window_done_layout").click()
-    time.sleep(5)
-    driver.quit()
-
-打开小米手机上的文件管理
+desired_caps = {
+                # 这里是声明android还是ios的环境
+                'platformName': 'Android',
+                # 手机设备名称，通过adb devices查看
+                'deviceName': '11642f40',
+                # android系统的版本号
+                'platformVersion': '6.0.1',
+                # apk包名
+                'appPackage': 'com.yibasan.lizhifm',
+                # apk的launcherActivity
+                'appActivity': 'com.yibasan.lizhifm.activities.EntryPointActivity'
+                }
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+# 休眠15秒等待页面加载完成
+time.sleep(10)
+# button = driver.find_elements_by_class_name("android.widget.LinearLayout")
+driver.find_element_by_id("com.yibasan.lizhifm:id/header_user_icon").click()
+driver.find_element_by_id("com.yibasan.lizhifm:id/followLabel").click()
+driver.find_element_by_id("com.yibasan.lizhifm:id/user_fans_user_head").click()
+# android.widget.TextView
+# 点击【声音】
+driver.find_elements_by_class_name("android.widget.TextView")[11].click()
+# 点击【下载】
+driver.find_element_by_id("com.yibasan.lizhifm:id/btn_download").click()
+# 找未下载项，提取标题
+tittle = driver.find_elements_by_id("com.yibasan.lizhifm:id/simple_program_item_text_name")
+radio = driver.find_elements_by_id("com.yibasan.lizhifm:id/view_select_status")
+for i in range(len(tittle)):
+    if radio[i].is_selected() == False:
+        print tittle[i].text
+    i = i+1
+# 点击【全选】
+driver.find_element_by_id("com.yibasan.lizhifm:id/view_select_all").click()
+# 点击【开始下载】
+driver.find_element_by_id("com.yibasan.lizhifm:id/download_pop_window_done_layout").click()
+time.sleep(5)
+driver.quit()
 
 
 
