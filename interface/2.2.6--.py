79f27718ff432c67a7310a062a1b8fc25b0916d5
uyaly <uyaly@qq.com>
2018-01-24 15:54:32 +0800

    interface 学习

--- interface/2.2.6--.py
+++ interface/2.2.6--.py
@@ -2,10 +2,10 @@
 import requests
 url = "https://passport.cnblogs.com/user/signin"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0",
-           "Accept": "application/json, text/javascript, */*; q=0.01",
-           "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
-           "Accept-Encoding": "gzip, deflate, br",
-           "Content-Type": "application/json; charset=utf-8",
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Content-Type": "application/json; charset=utf-8",
             # "VerificationToken": "",
             "X-Requested-With": "XMLHttpRequest",
             # "Referer": "",
@@ -13,6 +13,6 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20
             "Cookie": "xxx...",   # 此处省略
             "Connection": "keep-alive"
            }
-payload = {"input1":"xxx", "input2":"xxx", "remember":True}
-r = requests.post(url, json=payload, headers=headers,verify=False)
+payload = {"input1": "xxx", "input2": "xxx", "remember": True}
+r = requests.post(url, json=payload, headers=headers, verify=False)
 print r.json()
