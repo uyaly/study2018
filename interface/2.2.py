c041f7a0640e6202430f9b2862b58e810bae9f6c
uyaly <uyaly@qq.com>
2018-01-24 11:37:50 +0800

    interface 学习

---
+++ interface/2.2.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+import requests
+import json
+payload = {"yoyo": "hello world",
+           "pythonQQ群": "226666666"}
+# 转化成json格式
+data_json = json.dumps(payload)
+# r = requests.post("http://httpbin.org/post", data=payload)
+r = requests.post("http://httpbin.org/post", json=data_json)
+print r.text
