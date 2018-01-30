e4dc6c5d0302ababc43c64c79557189c8cb5be41
uyaly <uyaly@qq.com>
2018-01-30 20:43:57 +0800

    interface 学习2.8.5

---
+++ interface/2.8.5.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+import requests
+url = "http://www.kuaidi.com"
+headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0" }
+# get方法加个ser-Agent就可以了
+s = requests.session()
+r = s.get(url, headers=headers, verify=False)
+result = r.json()
+data = result["data"]
+# 获取data里面内容
+print data
+print data[0]
+#  获取data里最上面有个
+get_result = data[0]['context']
+# 获取已签收状态
+print get_result
+if u"已签收" in get_result:
+    print "快递单已签收成功"
+else:
+    print "未签收"
