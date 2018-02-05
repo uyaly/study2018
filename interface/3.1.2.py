3906260fa8b75e84d7ab02060d6a4a66955f0c68
uyaly <uyaly@qq.com>
2018-02-05 17:27:15 +0800

    自动化学习，3.4.3

---
+++ interface/3.1.2.py
@@ -0,0 +1,10 @@
+import unittest
+# print help(unittest)
+class IntegerArithmeticTestCase(unittest.TestCase):
+    def testAdd(self): ## test method names begin 'test*'
+        self.assertEqual((1 + 2), 3)
+        self.assertEqual(0 + 1, 1)
+    def testMultiply(self):
+        self.assertEqual((0 * 10), 0)
+        self.assertEqual((5 * 8), 40)
+if __name__ == '__main__': unittest.main()
