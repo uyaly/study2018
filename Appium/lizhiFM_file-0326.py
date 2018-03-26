b2337c594ee662805548d7a7f7325a0eddf0758a
uyaly <uyaly@qq.com>
2018-03-26 15:22:45 +0800

    修改荔枝FM的文件管理APP自动化

--- Appium/lizhiFM_file-0326.py
+++ Appium/lizhiFM_file-0326.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 # !/usr/bin/env Python
-
+# 修改为全部下载
 from appium import webdriver
 import time
 from appium.webdriver.common.touch_action import TouchAction
@@ -51,49 +51,13 @@ driver.find_element_by_id("com.yibasan.lizhifm:id/user_fans_user_head").click()
 # 点击进入【声音】
 driver.find_elements_by_class_name("android.widget.TextView")[11].click()
 # 点击进入【下载】
-download = driver.find_element_by_id("com.yibasan.lizhifm:id/btn_download")
-download.click()
-# 找未下载项，提取标题
-tittle = driver.find_elements_by_id("com.yibasan.lizhifm:id/simple_program_item_text_name")
-l = 0
-for j in range(7):
-    if j % 7 == 0 and j != 0:
-        swipeUpLiZhi(driver, n=2)
-        l = l+1
-    # t = tittle[j-7*l]
-    tittle[j-7*l].click()
-    # 点击【开始下载】
-    driver.find_element_by_id("com.yibasan.lizhifm:id/download_pop_window_done_layout").click()
-    try:
-    # 如果开始下载会返回，download可以点击，再次进入下载列表，记录标题；
-    # 否则抛异常，什么都做继续for循环
-        download.click()
-        titles.append(tittle[j-7*l].text)
-        print "第" + str(j+1) + "行已下载:" + (tittle[j-7*l].text)
-        # time.sleep(2)
-        # 执行xx1xx的点击动作，元素没有，会报错.如果元素存在则说明也不会发生
-    except:
-        if tittle[j-7*l].text in titleAll and tittle[j-7*l].text is not "":
-            print "第" + str(j+1) + "行本次已重复:" + tittle[j-7*l].text
-        else:
-            titleAll.append(tittle[j-7*l].text)
-            print "第" + str(j+1) + "行未下载:" + (tittle[j-7*l].text)
-            # break   # 遇到第一个未下载项，跳出for循环
-titles = [i + ".mp3" for i in reversed(titles)]
-print titles
-# 增加一个正在下载为0的判断
-for i in range(0, 60):
-    try:
-        if driver.find_element_by_name("正在下载(0)").is_displayed():
-            # print "正在下载0"
-            # 关闭荔枝FM
-            driver.quit()
-        else:
-            print "控件未出现,等待1秒"
-            time.sleep(1)
-    except:
-        pass
+driver.find_element_by_id("com.yibasan.lizhifm:id/btn_download").click()
+# 全选
+driver.find_elements_by_id("com.yibasan.lizhifm:id/view_select_all")
+# 开始下载
+driver.find_element_by_id("com.yibasan.lizhifm:id/download_pop_window_done_layout").click()
 driver.quit()
+
 time.sleep(10)
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps2)
 # 休眠15秒等待页面加载完成
