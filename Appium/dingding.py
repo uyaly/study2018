233597d9bd55036d0b64bca0330407e05af8988a
uyaly <uyaly@qq.com>
2018-04-18 08:40:19 +0800

    修改dingding自动化

--- Appium/dingding.py
+++ Appium/dingding.py
@@ -1,6 +1,11 @@
 # coding:utf-8
 from appium import webdriver
+import datetime
 import time
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+t = datetime.datetime.now()
 desired_caps = {
                 # 这里是声明android还是ios的环境
                 'platformName': 'Android',
@@ -14,21 +19,34 @@ desired_caps = {
                 'appActivity': 'com.alibaba.android.rimet.biz.SplashActivity'
                 }
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
-
 time.sleep(5)
+
 try:
     # 输入密码
     driver.find_element_by_id("com.alibaba.android.rimet:id/et_pwd_login").send_keys("123456")
     # 点击登录按钮
     driver.find_element_by_id("com.alibaba.android.rimet:id/btn_next").click()
 except:
+    print(u"默认登录")
     pass
 time.sleep(5)
+
 driver.find_element_by_id("com.alibaba.android.rimet:id/home_bottom_tab_button_work").click()
 driver.find_element_by_android_uiautomator('new UiSelector().description("考勤打卡")').click()
-# print driver.context
 time.sleep(10)
-driver.find_element_by_android_uiautomator('new UiSelector().description("下班打卡")').click()
-# driver.find_element_by_accessibility_id("下班打卡").click()
+if (t.hour < 8 or t.hour == 8):
+    try:
+        driver.find_element_by_android_uiautomator('new UiSelector().description("上班打卡")').click()
+        print(u"上班手动打卡成功")
+    except:
+        print(u"上班自动打卡成功")
+        pass
+else:
+    try:
+        driver.find_element_by_android_uiautomator('new UiSelector().description("下班打卡")').click()
+        print(u"下班手动打卡成功")
+    except:
+        print(u"无操作")
+        pass
 time.sleep(5)
 driver.quit()
--- Appium/test2_1.py
+++ Appium/test2_1.py
@@ -1,28 +1,7 @@
 # coding:utf-8
-# coding:gb18030
-from appium import webdriver
-import time
-desired_caps = {
-                # 这里是声明android还是ios的环境
-                'platformName': 'Android',
-                # 手机设备名称，通过adb devices查看
-                'deviceName': '11642f40',
-                # android系统的版本号
-                'platformVersion': '6.0.1',
-                # apk包名
-                'appPackage': 'com.taobao.taobao',
-                # apk的launcherActivity
-                'appActivity': 'com.taobao.tao.welcome.Welcome',
-                # unicodeKeyboard是使用unicode编码方式发送字符串
-                'unicodeKeyboard': True,
-                # resetKeyboard是将键盘隐藏起来
-                'resetKeyboard': True
-                }
-driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
-# 休眠15秒等待页面加载完成
-time.sleep(10)
-driver.find_element_by_id("com.taobao.taobao:id/home_searchedit").click()
-time.sleep(2)
-driver.find_element_by_id("com.taobao.taobao:id/searchEdit").click()
-driver.find_element_by_id("com.taobao.taobao:id/searchEdit").send_keys(u"测试部落")
-driver.quit()
+import datetime
+
+t = datetime.datetime.now()
+
+if (t.hour> 8):
+    print t.hour
