b81cd2a416c5b40f3078c2d53e31ba8a2ae9dfdf
uyaly <uyaly@qq.com>
2018-08-14 08:21:23 +0800

    update

--- Appium/dingding.py
+++ Appium/dingding.py
@@ -72,7 +72,6 @@ if (t.hour < 10 and t.hour > 7):
     try:
         driver.wait_activity("上班打卡", 5)
         driver.find_element_by_android_uiautomator('new UiSelector().description("上班打卡")').click()
-        driver.tap([(440, 1500)], 10)  # 确认打卡成功
         print("*** Go to work, Manual punch the clock, success at" + str(t) + "***")
     except:
         print("*** Go to work, quickly punch the clock, success at" + str(t) + "***")
@@ -81,13 +80,11 @@ elif (t.hour < 22 and t.hour >= 17):
     try:
         driver.wait_activity("下班打卡", 5)
         driver.find_element_by_android_uiautomator('new UiSelector().description("下班打卡")').click()
-        driver.tap([(440, 1500)], 10)  # 确认打卡成功
         print("*** Go off work, Manual punch the clock, success at" + str(t) + "***")
     except:
         driver.wait_activity("更新打卡", 5)
         driver.find_element_by_android_uiautomator('new UiSelector().description("更新打卡")').click()
         driver.find_element_by_name(u"确定").click()
-        driver.tap([(440, 1500)], 10)  # 确认打卡成功
         print("*** Go off work, Update punch the clock, success at" + str(t) + "***")
         # print("*** No operation ***")
         pass
@@ -95,6 +92,10 @@ else:
     print("*** No operation ***")
     pass
 
+try:
+    driver.tap([(440, 1500)], 10)  # 确认打卡成功
+except:
+    pass
 # 登出
 driver.wait_activity("返回", 5)
 driver.find_element_by_name(u"返回").click()
@@ -103,4 +104,5 @@ driver.swipe(0, 1622, 0, 530, 500)
 driver.find_element_by_id("com.alibaba.android.rimet:id/rl_setting").click() # 设置
 driver.find_element_by_name(u"退出登录").click()
 driver.find_element_by_name(u"确认").click()
+# 结束
 driver.quit()
