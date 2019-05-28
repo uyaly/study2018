42a870cb42abda0e30dede9d2e314b61aee7b33e
uyaly <uyaly@qq.com>
2019-05-28 18:19:07 +0800

    dd

--- python/笛卡尔算法/color2.py
+++ python/笛卡尔算法/color2.py
@@ -1,6 +1,6 @@
 # coding:UTF-8
 # 小明口袋有4个球，分别是红色，红色，黄色，蓝色。每次随机取出一个球记下颜色，然后再放回口袋，小明一共取了两次球，不考虑顺序，请给出所有颜色组合
-
+# 正解
 from itertools import product
 s = []
 for x,y in product(['r','r','y','b'],['r','r','y','b']):
