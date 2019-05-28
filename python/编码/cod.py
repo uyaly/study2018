886badef0313ecce95024b6ec231be103963685e
uyaly <uyaly@qq.com>
2019-05-28 19:26:27 +0800

    dd

---
+++ python/编码/cod.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+import sys
+# 获得系统的默认编码
+print sys.getdefaultencoding()
+
+# s = '\xe9\x9d\x92\xe8\x9b\x99\xe7\x8e\x8b\xe5\xad\x90'
+# ss = s.encode('raw_unicode_escape')
+# print(ss)  # 结果：b'\xe9\x9d\x92\xe8\x9b\x99\xe7\x8e\x8b\xe5\xad\x90'
+# sss = ss.decode()
+# print(sss)
+
+# s=u"中文"
+#
+# print s
+# print s.encode('gb2312')
+# print s.encode('utf8')
